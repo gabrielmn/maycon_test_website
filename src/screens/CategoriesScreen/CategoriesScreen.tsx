62ec854b280610030f9af9796989b5cb6c52bd31
Gabriel M. Nori <gabriel.m.nori@hotmail.com>
2021-12-10 18:13:20 -0300

    Categories screen added.

---
+++ src/screens/CategoriesScreen/CategoriesScreen.tsx
@@ -0,0 +1,94 @@
+import React, { Component } from "react";
+import Button from '../../components/Button/Button';
+import FileInputLayout from "../../components/FileInputLayout/FileInputLayout";
+import TextInputLayout from "../../components/TextInputLayout/TextInputLayout";
+import Text from '../../components/Text/Text';
+import classes from './CategoriesScreen.module.css'
+
+interface Props {
+
+}
+
+interface State {
+    open: boolean,
+    name: string,
+    file: any,
+
+}
+
+export default class ProductsScreen extends Component<Props, State> {
+
+    constructor(props: Props | Readonly<Props>) {
+        super(props);
+
+        this.state = {
+            open: false,
+            name: '',
+            file: null,
+        }
+
+        this.onClickAdd = this.onClickAdd.bind(this);
+        this.onChangeName = this.onChangeName.bind(this);
+        this.onChangeFile = this.onChangeFile.bind(this);
+        this.onCancelClick = this.onCancelClick.bind(this);
+
+    }
+
+
+    onClickAdd(): void {
+        this.setState((currentState) => {
+            return {
+                open: true
+            }
+        })
+    }
+
+    onChangeName(event: any): void {
+        this.setState((current) => {
+            return {
+                name: event.target.value
+            }
+        })
+    }
+
+    onChangeFile(event: any): void {
+        this.setState((current) => {
+            return {
+                file: event.target.value
+            }
+        })
+    }
+
+    onCancelClick() {
+        this.setState((current) => {
+            return {
+                open: false,
+                categoryId: -1,
+                name: '',
+                file: null
+            }
+        })
+    }
+
+    render(): React.ReactNode {
+        return (
+            <div className={classes.container}>
+                <div className={classes.card}>
+                    <Text>New Category</Text>
+                    <TextInputLayout
+                        className={classes.card_input}
+                        label="Name"
+                        type="string"
+                        value={this.state.name}
+                        onChange={this.onChangeName}
+                    />
+                    <FileInputLayout />
+                    <div>
+                        <Button onClick={() => { }}>Cancel</Button>
+                        <Button onClick={() => { }}>Confirm</Button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
