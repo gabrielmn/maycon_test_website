9da4a36c50754267c30d7892ff522cb8bee47a15
Gabriel M. Nori <gabriel.m.nori@hotmail.com>
2021-12-10 18:12:50 -0300

    Panel container added.

---
+++ src/container/PanelContainer/PanelContainer.tsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import NavigationDrawer from '../../components/NavigationDrawer/NavigationDrawer';
+import ProductsScreen from '../../screens/ProductsScreen/ProductsScreen';
+import CategoriesScreen from '../../screens/CategoriesScreen/CategoriesScreen'
+import classes from './PanelContainer.module.css';
+
+export default class PanelContainer extends Component {
+
+    render() {
+        return (
+            <div className={classes.container}>
+                <NavigationDrawer />
+                <main className={classes.main}>
+                    <Switch>
+                        <Route path='/panel' exact>
+                        </Route>
+                        <Route path='/panel/products' exact>
+                            <ProductsScreen/>
+                        </Route>
+                        <Route path='/panel/categories' exact>
+                            <CategoriesScreen/>
+                        </Route>
+                        <Route path='*'>
+                            <Redirect to='/not-found' />
+                        </Route>
+                    </Switch>
+                </main>
+            </div>
+        )
+    }
+}
